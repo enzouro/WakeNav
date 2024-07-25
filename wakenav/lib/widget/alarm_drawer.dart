f3452fed190de8bc273cfd7eeea359e1b3548fc7
Genmaa <mtalarcon1@up.edu.ph>
2024-07-26 04:20:27 +0800

    added validator for entering alarm name

--- wakenav/lib/widget/alarm_drawer.dart
+++ wakenav/lib/widget/alarm_drawer.dart
@@ -34,6 +34,7 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
   TextEditingController _noteController = TextEditingController();
   DraggableScrollableController _draggableController =
       DraggableScrollableController();
+  final _formKey = GlobalKey<FormState>();
 
   final double _collapsedSize = 0.32;
   final double _expandedSize = 0.45;
@@ -71,10 +72,31 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
   }
 
   Future<void> _saveAlarm({bool setActive = false}) async {
+  if (_formKey.currentState?.validate() ?? false) {
     final prefs = await SharedPreferences.getInstance();
+    final String? alarmsJson = prefs.getString('alarms');
+    List<Alarm> alarms = [];
+    
+    if (alarmsJson != null) {
+      final List<dynamic> decodedAlarms = jsonDecode(alarmsJson);
+      alarms = decodedAlarms.map((e) => Alarm.fromJson(e)).toList();
+    }
+
+    final newAlarmName = _alarmNameController.text.trim();
+
+    // Check for duplicate alarm names
+    bool nameExists = alarms.any((alarm) => alarm.name == newAlarmName);
+    if (nameExists) {
+      // Show an error message or handle the duplicate name case
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An alarm with this name already exists.')),
+      );
+      return;
+    }
+
     final alarm = Alarm(
       id: Uuid().v4(),
-      name: _alarmNameController.text,
+      name: newAlarmName,
       distance: _distance,
       note: _noteController.text,
       latitude: widget.latitude,
@@ -82,22 +104,11 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
       isActive: setActive,
     );
 
-    // Get existing alarms
-    final String? alarmsJson = prefs.getString('alarms');
-    List<Alarm> alarms = [];
-    if (alarmsJson != null) {
-      final List<dynamic> decodedAlarms = jsonDecode(alarmsJson);
-      alarms = decodedAlarms.map((e) => Alarm.fromJson(e)).toList();
-    }
-
-    // Add new alarm
     alarms.add(alarm);
 
-    // Save updated alarms list
     await prefs.setString(
         'alarms', jsonEncode(alarms.map((e) => e.toJson()).toList()));
 
-    // Close the drawer
     widget.onCancel();
 
     if (setActive) {
@@ -108,6 +119,7 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
     }
     widget.onCancel();
   }
+}
 
   _startAlarm() {
     _saveAlarm(setActive: true);
@@ -134,46 +146,48 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
               ),
             ],
           ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Set Alarm',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500)),
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                              _isExpanded
-                                  ? Icons.expand_more
-                                  : Icons.expand_less,
-                              color: Colors.white),
-                          onPressed:
-                              _toggleExpansion, // Use the new _toggleExpansion method
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.close, color: Colors.white),
-                          onPressed: () => widget.onCancel(),
-                        ),
-                      ],
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Set Alarm',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500)),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                                _isExpanded
+                                    ? Icons.expand_more
+                                    : Icons.expand_less,
+                                color: Colors.white),
+                            onPressed:
+                                _toggleExpansion, // Use the new _toggleExpansion method
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.close, color: Colors.white),
+                            onPressed: () => widget.onCancel(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: ListView(
-                  controller: controller,
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  children: [
-                    TextFormField(
-                      controller: _alarmNameController,
-                      decoration: InputDecoration(
+                Expanded(
+                  child: ListView(
+                    controller: controller,
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    children: [
+                      TextFormField(
+                        controller: _alarmNameController,
+                        decoration: InputDecoration(
                           contentPadding: EdgeInsets.all(20),
                           border: OutlineInputBorder(),
                           focusedBorder: OutlineInputBorder(
@@ -192,74 +206,85 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
                           filled: true,
                           fillColor: Colors.grey[100],
                           // label: Text("Alarm Name"),
-                          hintText: "Set Alarm Name"),
-                    ),
-                    SizedBox(height: 15),
-                    Row(
-                      children: [
-                        Text('Distance: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w500)),
-                        Text('${_distance.toStringAsFixed(0)} m',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal)),
-                      ],
-                    ),
-                    Slider(
-                      value: _distance,
-                      min: 50,
-                      max: 5000,
-                      divisions: 19,
-                      onChanged: (value) {
-                        setState(() {
-                          _distance = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          width: 180,
-                          child: Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: _saveAlarm,
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                child: Text('Save',
-                                    style: TextStyle(color: Color(0xFF008080))),
-                              ),
-                              SizedBox(width: 15),
-                              ElevatedButton(
-                                onPressed: _startAlarm,
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                child: Text('Start',
-                                    style: TextStyle(color: Color(0xFF008080))),
-                              ),
-                            ],
+                          hintText: "Set Alarm Name",
+                          hintStyle: TextStyle(
+                            color:
+                                Color.fromARGB(255, 28, 27, 27).withOpacity(0.5),
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 15),
-                    if (_isExpanded) ...[
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a name';
+                          }
+                          return null;
+                        },
+                      ),
                       SizedBox(height: 15),
-                      TextFormField(
-                        controller: _noteController,
-                        decoration: InputDecoration(
+                      Row(
+                        children: [
+                          Text('Distance: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500)),
+                          Text('${_distance.toStringAsFixed(0)} m',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.normal)),
+                        ],
+                      ),
+                      Slider(
+                        value: _distance,
+                        min: 50,
+                        max: 5000,
+                        divisions: 19,
+                        onChanged: (value) {
+                          setState(() {
+                            _distance = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            width: 180,
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: _saveAlarm,
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: Text('Save',
+                                      style: TextStyle(color: Color(0xFF008080))),
+                                ),
+                                SizedBox(width: 15),
+                                ElevatedButton(
+                                  onPressed: _startAlarm,
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: Text('Start',
+                                      style: TextStyle(color: Color(0xFF008080))),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      if (_isExpanded) ...[
+                        SizedBox(height: 15),
+                        TextFormField(
+                          controller: _noteController,
+                          decoration: InputDecoration(
                             contentPadding: EdgeInsets.all(20),
                             border: OutlineInputBorder(),
                             focusedBorder: OutlineInputBorder(
@@ -278,14 +303,20 @@ class _AlarmDrawerState extends State<AlarmDrawer> {
                             filled: true,
                             fillColor: Colors.grey[100],
                             // label: Text("Alarm Name"),
-                            hintText: "Details"),
-                        maxLines: 3,
-                      ),
+                            hintText: "Details",
+                            hintStyle: TextStyle(
+                              color: Color.fromARGB(255, 28, 27, 27)
+                                  .withOpacity(0.5),
+                            ),
+                          ),
+                          maxLines: 3,
+                        ),
+                      ],
                     ],
-                  ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
